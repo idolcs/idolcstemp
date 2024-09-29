21ee111806967a252437c35e343b5a796f84ed69
Yash Kolambekar <41070233+yashkolambekar@users.noreply.github.com>
2024-09-29 22:58:42 +0530

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <meta name="google-adsense-account" content="ca-pub-7148141514273746">
     @viteReactRefresh
     @vite('resources/js/app.jsx')
     @inertiaHead
