cede5ff3aa4832c96d1aa1e550fcaa51b1834cf5
yashkolambekar <yashskolambekar2015@gmail.com>
2024-10-09 21:12:30 +0530

    removed googleads tag and added umami tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/svg+xml" href="../../assets/IDOLCS.svg">
     <title>IDOL CS</title>
-    <meta name="google-adsense-account" content="ca-pub-7148141514273746">
+    <script defer src="https://analytics.yassh.in/script.js" data-website-id="b327e439-7ba8-4534-9af3-b1ece36624be"></script>
     @viteReactRefresh
     @vite('resources/js/app.jsx')
     @inertiaHead
