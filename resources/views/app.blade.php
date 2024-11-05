a82ae83214d43d06dc9dbe1c22bb642a9f3b34b8
yashkolambekar <yashskolambekar2015@gmail.com>
2024-11-05 19:27:29 +0530

    changed tracking code from yassh.in to 000000108.xyz

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/svg+xml" href="../../assets/IDOLCS.svg">
     <title>IDOL CS</title>
-    <script defer src="https://analytics.yassh.in/script.js" data-website-id="b327e439-7ba8-4534-9af3-b1ece36624be"></script>
+    <script defer src="https://umami.000000108.xyz/script.js" data-website-id="5410d6b2-4527-472f-a18b-37f4fa712115"></script>
     @viteReactRefresh
     @vite('resources/js/app.jsx')
     @inertiaHead
