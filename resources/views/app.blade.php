9a52827d6fecaad2f8a78e142b06a82d269a7dd7
Yash <41070233+yashkolambekar@users.noreply.github.com>
2025-05-02 12:56:15 +0530

    removed uammi

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/svg+xml" href="../../assets/IDOLCS.svg">
     <title>IDOL CS</title>
-    <script defer src="https://umami.000000108.xyz/script.js" data-website-id="5410d6b2-4527-472f-a18b-37f4fa712115"></script>
     @viteReactRefresh
     @vite('resources/js/app.jsx')
     @inertiaHead
